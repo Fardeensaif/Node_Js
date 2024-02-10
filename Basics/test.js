ef187ffbec9788a03293138ea67d216f3577a052
Fardeen khan <104503306+Fardeensaif@users.noreply.github.com>
2024-02-10 07:13:05 +0000

    done

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,12 @@
-console.log("fardeen khan");
+function generateOTP(length) {
+    const characters = '0123952718699716';
+    let OTP = '';
+    for (let i = 0; i < length; i++) {
+        OTP += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return OTP;
+}
+
+// Example usage:
+const otp = generateOTP(4); // Generates a 6-digit OTP
+console.log(otp);
--- Basics/var.js
+++ Basics/var.js
@@ -1,4 +1,22 @@
-console.log("fardeen khan hu maen");
-const name="akib";
-let last="khan";
-console.table[name,last];
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengal";
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
